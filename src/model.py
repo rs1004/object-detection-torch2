0970b97fa7b548c67a068541146059f27c079f10
rs1004 <r82.y25@gmail.com>
2020-07-07 13:03:43 +0000

    [fix] vgg16

--- src/model.py
+++ src/model.py
@@ -1,13 +1,86 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class VGG16(nn.Module):
-    def __init__(self, is_train=True):
-        pass
+    def __init__(self, class_num, is_train=True):
+        # initialize
+        self.is_train = is_train
+
+        # layer1
+        self.conv1_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1)
+        self.conv1_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # layer2
+        self.conv2_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.conv2_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # layer3
+        self.conv3_1 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
+        self.conv3_2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.conv3_3 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # layer4
+        self.conv4_1 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1)
+        self.conv4_2 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.conv4_3 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # layer5
+        self.conv5_1 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.conv5_2 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.conv5_3 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.pool5 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # layer6
+        self.fc6_1 = nn.Linear(in_features=1024 * 10 * 10, out_features=4096)
+        self.fc6_2 = nn.Linear(in_features=4096, out_features=4096)
+        self.fc6_3 = nn.Linear(in_features=4096, out_features=class_num)
 
     def forward(self, x):
-        pass
+        # layer1
+        x = F.relu(self.conv1_1(x))
+        x = F.relu(self.conv1_2(x))
+        x = self.pool1(x)
+
+        # layer2
+        x = F.relu(self.conv2_1(x))
+        x = F.relu(self.conv2_2(x))
+        x = self.pool2(x)
+
+        # layer3
+        x = F.relu(self.conv3_1(x))
+        x = F.relu(self.conv3_2(x))
+        x = F.relu(self.conv3_3(x))
+        x = self.pool3(x)
+
+        # layer4
+        x = F.relu(self.conv4_1(x))
+        x = F.relu(self.conv4_2(x))
+        x = F.relu(self.conv4_3(x))
+        x = self.pool4(x)
+
+        # layer5
+        x = F.relu(self.conv5_1(x))
+        x = F.relu(self.conv5_2(x))
+        x = F.relu(self.conv5_3(x))
+        x = self.pool5(x)
+
+        # layer6
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc6_1(x))
+        if self.is_train:
+            x = nn.Dropout(p=0.2)(x)
+        x = F.relu(self.fc6_2(x))
+        if self.is_train:
+            x = nn.Dropout(p=0.2)(x)
+        x = self.fc6_3(x)
+
+        return x
 
     def num_flat_features(self, x):
         size = x.size()[1:]
