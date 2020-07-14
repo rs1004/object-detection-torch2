e583847ea65ece5bfbf8830294161d0932ad8d9f
rs1004 <r82.y25@gmail.com>
2020-07-14 13:27:51 +0000

    [add] lossクラス追加

---
+++ src/loss.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class SSDLoss(nn.Module):
+    def __init__(self):
+        # initialize
+        super(SSDLoss, self).__init__()
+
+    def forward(self, inputs, targets):
+        pass
