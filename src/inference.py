eae0676da89eb79e26f56ec3abb483754abafb1d
rs1004 <r82.y25@gmail.com>
2020-09-08 12:35:53 +0000

    [fix] 関数の移動

--- src/inference.py
+++ src/inference.py
@@ -1,110 +1,13 @@
 from dataset import PascalVOCDataset
 from model import SSD
 from augmentation import Compose, ToTensor
-from utils import collate_fn, LabelMap
+from utils import collate_fn, calc_coordicate, calc_score, non_maximum_suppression, LabelMap
 from pathlib import Path
 from tqdm import tqdm
 from PIL import Image, ImageDraw
 import seaborn as sns
 import torch
-import torch.nn.functional as F
 import argparse
-
-
-def calc_coordicate(pr: torch.Tensor, df: torch.Tensor) -> torch.Tensor:
-    """calculate pred-bbox coordinate
-
-    Args:
-        pr (torch.Tensor): (N, P, 4)
-        df (torch.Tensor): (P, 4) -> (1, P, 4)
-
-    Returns:
-        torch.Tensor (N, P, 4): location coordinate
-    """
-    df = df.unsqueeze(0)
-
-    p_cx, p_cy, p_w, p_h = [pr[:, :, i] for i in range(4)]
-    d_cx, d_cy, d_w, d_h = [df[:, :, i] for i in range(4)]
-
-    # Δ(cx, cy, w, h) => (cx, cy, w, h)
-    p_cx = d_w * p_cx + d_cx
-    p_cy = d_h * p_cy + d_cy
-    p_w = d_w * torch.exp(p_w)
-    p_h = d_h * torch.exp(p_h)
-
-    return torch.stack([p_cx, p_cy, p_w, p_h], dim=2)
-
-
-def calc_score(pr: torch.Tensor) -> torch.Tensor:
-    """calculate pred-bbox score
-
-    Args:
-        pr (torch.Tensor): (N, P, C)
-
-    Returns:
-        torch.Tensor (N, P, C-4): class score
-    """
-    _, _, C = pr.shape
-
-    max_mask = torch.eye(C - 4)[torch.max(pr[:, :, 4:], dim=2).indices].to(pr.device)
-    return F.softmax(pr[:, :, 4:], dim=2) * max_mask
-
-
-def calc_iou(t: torch.Tensor, s: torch.Tensor) -> torch.Tensor:
-    """calculate iou
-
-    Args:
-        t (torch.Tensor): (N, T, 4) -> (N, T, 1, 4)
-        s (torch.Tensor): (N, S, 4) -> (N, 1, S, 4)
-
-    Returns:
-        torch.Tensor: (N, T, S)
-    """
-    t = t.unsqueeze(2)
-    s = s.unsqueeze(1)
-
-    t_cx, t_cy, t_w, t_h = [t[:, :, :, i] for i in range(4)]
-    s_cx, s_cy, s_w, s_h = [s[:, :, :, i] for i in range(4)]
-
-    w = (torch.min(t_cx + t_w / 2, s_cx + s_w / 2) - torch.max(t_cx - t_w / 2, s_cx - s_w / 2)).clamp(min=0)
-    h = (torch.min(t_cy + t_h / 2, s_cy + s_h / 2) - torch.max(t_cy - t_h / 2, s_cy - s_h / 2)).clamp(min=0)
-
-    return torch.where(w * h > 0, w * h / (t_w * t_h + s_w * s_h - w * h), w * h)
-
-
-def non_maximum_suppression(outputs: torch.Tensor, iou_thresh: float = 0.5) -> torch.Tensor:
-    """execute non-maximum-suppression
-
-    Args:
-        outputs (torch.Tensor): (N, P, C)
-        iou_thresh (float, optional): Remove predicted values ​​above this threshold. Defaults to 0.5.
-
-    Returns:
-        torch.Tensor: (N, P, C)
-    """
-    def nms(t: torch.Tensor) -> torch.Tensor:
-        """suppress tensor
-
-        Args:
-            t (torch.Tensor): (N, P, C-4, P)
-
-        Returns:
-            torch.Tensor: (N, P, C-4)
-        """
-        suppressed_t = t[:, :, :, 0]
-        for i in range(1, P):
-            t[:, :, :, i] *= suppressed_t
-            suppressed_t += t[:, :, :, i]
-        return suppressed_t
-
-    N, P, _ = outputs.shape
-
-    select_flags = iou_thresh < calc_iou(outputs, outputs) + torch.eye(P).bool().to(outputs.device)
-    orders = torch.sort(outputs[:, :, 4:], dim=1, descending=True).indices
-    valid_mask = nms(t=torch.stack([select_flags[n][orders[n]] for n in range(N)]))
-    outputs[:, :, 4:] = outputs[:, :, 4:] * valid_mask
-
-    return outputs
 
 
 if __name__ == '__main__':
@@ -173,11 +76,11 @@ if __name__ == '__main__':
                     locs = bbox_locs[i]
                     labels = torch.max(bbox_confs[i], dim=1)
                     for loc, class_id, score in zip(locs, labels.indices, labels.values):
-                        # case: void or noise
+                        # case: void
                         if class_id == 0:
                             continue
 
-                        # calc coord
+                        # calc location
                         cx, cy, w, h = [_.item() for _ in loc * args.imsize]
                         xmin, ymin, xmax, ymax = cx - w / 2, cy - h / 2, cx + w / 2, cy + h / 2
                         left_top = (max(xmin, 0), max(ymin, 0))
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import json
 import torch
+import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_sequence
 from pathlib import Path
 
@@ -13,6 +14,102 @@ def collate_fn(batch):
     images = torch.stack(images, dim=0)
     gts = pad_sequence(gts, batch_first=True)
     return images, gts
+
+
+def calc_coordicate(pr: torch.Tensor, df: torch.Tensor) -> torch.Tensor:
+    """calculate pred-bbox coordinate
+
+    Args:
+        pr (torch.Tensor): (N, P, 4)
+        df (torch.Tensor): (P, 4) -> (1, P, 4)
+
+    Returns:
+        torch.Tensor (N, P, 4): location coordinate
+    """
+    df = df.unsqueeze(0)
+
+    p_cx, p_cy, p_w, p_h = [pr[:, :, i] for i in range(4)]
+    d_cx, d_cy, d_w, d_h = [df[:, :, i] for i in range(4)]
+
+    # Δ(cx, cy, w, h) => (cx, cy, w, h)
+    p_cx = d_w * p_cx + d_cx
+    p_cy = d_h * p_cy + d_cy
+    p_w = d_w * torch.exp(p_w)
+    p_h = d_h * torch.exp(p_h)
+
+    return torch.stack([p_cx, p_cy, p_w, p_h], dim=2)
+
+
+def calc_score(pr: torch.Tensor) -> torch.Tensor:
+    """calculate pred-bbox score
+
+    Args:
+        pr (torch.Tensor): (N, P, C)
+
+    Returns:
+        torch.Tensor (N, P, C-4): class score
+    """
+    _, _, C = pr.shape
+
+    max_mask = torch.eye(C - 4)[torch.max(pr[:, :, 4:], dim=2).indices].to(pr.device)
+    return F.softmax(pr[:, :, 4:], dim=2) * max_mask
+
+
+def calc_iou(t: torch.Tensor, s: torch.Tensor) -> torch.Tensor:
+    """calculate iou
+
+    Args:
+        t (torch.Tensor): (N, T, 4) -> (N, T, 1, 4)
+        s (torch.Tensor): (N, S, 4) -> (N, 1, S, 4)
+
+    Returns:
+        torch.Tensor: (N, T, S)
+    """
+    t = t.unsqueeze(2)
+    s = s.unsqueeze(1)
+
+    t_cx, t_cy, t_w, t_h = [t[:, :, :, i] for i in range(4)]
+    s_cx, s_cy, s_w, s_h = [s[:, :, :, i] for i in range(4)]
+
+    w = (torch.min(t_cx + t_w / 2, s_cx + s_w / 2) - torch.max(t_cx - t_w / 2, s_cx - s_w / 2)).clamp(min=0)
+    h = (torch.min(t_cy + t_h / 2, s_cy + s_h / 2) - torch.max(t_cy - t_h / 2, s_cy - s_h / 2)).clamp(min=0)
+
+    return torch.where(w * h > 0, w * h / (t_w * t_h + s_w * s_h - w * h), w * h)
+
+
+def non_maximum_suppression(outputs: torch.Tensor, iou_thresh: float = 0.5) -> torch.Tensor:
+    """execute non-maximum-suppression
+
+    Args:
+        outputs (torch.Tensor): (N, P, C)
+        iou_thresh (float, optional): Remove predicted values ​​above this threshold. Defaults to 0.5.
+
+    Returns:
+        torch.Tensor: (N, P, C)
+    """
+    def nms(t: torch.Tensor) -> torch.Tensor:
+        """suppress tensor
+
+        Args:
+            t (torch.Tensor): (N, P, C-4, P)
+
+        Returns:
+            torch.Tensor: (N, P, C-4)
+        """
+        suppressed_t = t[:, :, :, 0]
+        for i in range(1, P):
+            t[:, :, :, i] *= suppressed_t
+            suppressed_t += t[:, :, :, i]
+        return suppressed_t
+
+    N, P, _ = outputs.shape
+
+    select_flags = iou_thresh < calc_iou(outputs, outputs) + torch.eye(P).bool().to(outputs.device)
+    orders = torch.sort(outputs[:, :, 4:], dim=1, descending=True).indices
+    valid_mask = nms(t=torch.stack([select_flags[n][orders[n]] for n in range(N)]))
+    outputs[:, :, 4:] = outputs[:, :, 4:] * valid_mask
+
+    return outputs
 
 
 class LabelMap:
